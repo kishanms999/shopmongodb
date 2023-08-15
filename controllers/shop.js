18f7815a753546d502a1cd14c82fff1e3d549c98
kishanms999 <kishanms99@gmail.com>
2023-08-15 19:52:18 +0530

    cart system

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,8 +45,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -55,6 +57,7 @@ exports.getCart = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
